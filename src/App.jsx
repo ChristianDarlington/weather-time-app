b0ee7f2ac0bcd7d4e06745175b96a10f619a81e1
Christian <christiandarlington0@gmail.com>
2023-10-26 23:34:49 -0400

    backgroud vid added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import CurrentWeather from './components/CurrentWeather'
 // import Forecast from "./components/Forecast"
 // import HourlyWeather from "./components/HourlyWeather"
 
-// import Rainy from './assets/Rainy.mp4'
+import Rainy from './assets/Rainy.mp4'
 // import Sunny from './assets/Sunny.jpeg'
 // import API_KEY from ''
 
@@ -35,8 +35,8 @@ function App() {
 
   return (
     <div className="relative w-full h-screen">
-      {/* <video className="w-full h-full object-cover" src={Rainy} autoPlay loop muted /> */}
-      {/* <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[2]" /> */}
+      <video className="w-full h-full object-cover" src={Rainy} autoPlay loop muted />
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[2]" />
       {weatherData?.city?.name || ''}
       <br />
       {weatherData?.list?.map((row) => (
@@ -48,7 +48,6 @@ function App() {
               {r.main} - {r.description}
             </>
           ))}
-          <hr />
         </>
       ))}
 
